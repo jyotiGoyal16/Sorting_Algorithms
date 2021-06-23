7d6f022206105c3f69b13aff47bd40588585d757
Jyoti Goyal <56450138+jyotiGoyal16@users.noreply.github.com>
2021-06-23 23:17:18 +0530

    Update MergeSort.js

--- MergeSort.js
+++ MergeSort.js
@@ -86,3 +86,50 @@ callMergeSort();
   mergeSort(arr, 0, arr.length-1);
   console.log(arr);
 })();
+
+
+//METHOD 3
+//Without IIFE
+
+function merge(arr, l, m, r){
+  let n1 = m-l+1,
+      n2 = r-m,
+      L = new Array(n1),
+      R = new Array(n2),
+      i, j, k=l;
+  
+  for(i=0; i<n1; i++)
+    L[i] = arr[l+i];
+  
+  for(j=0; j<n2; j++)
+    R[j] = arr[m+j+1];
+  
+  i=0; 
+  j=0;
+  
+  while(i<n1 && j<n2){
+    if(L[i] <= R[j])
+      arr[k++] = L[i++];
+    else
+      arr[k++] = R[j++];
+  }
+  
+  while(i<n1)
+    arr[k++] = L[i++];
+  
+  while(j<n2)
+    arr[k++] = R[j++];
+}
+
+function mergeSort(arr, l, r){
+  if(l>=r)
+    return;
+  let m = l + Math.floor((r-l)/2);
+  mergeSort(arr, l, m);
+  mergeSort(arr, m+1, r);
+  merge(arr, l, m, r);
+}
+
+let arr = [38, 27, 43, 3, 9, 82, 10]; 
+mergeSort(arr, 0, arr.length - 1);
+console.log(arr);
