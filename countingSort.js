36fed0c3ceca31df0b7f5b836fa348829e995375
Jyoti Goyal <56450138+jyotiGoyal16@users.noreply.github.com>
2021-06-26 15:41:29 +0530

    Update countingSort.js

--- countingSort.js
+++ countingSort.js
@@ -38,3 +38,33 @@ function countSort(arr, n){
 const arr = ['g', 'e', 'e', 'k', 's', 'f', 'o', 'r', 'g', 'e', 'e', 'k', 's'];
 countSort(arr, arr.length);
 console.log(arr);
+
+
+//Below code will work for numbers(positive/negative) array
+function countSort(arr, n){
+  let max = Math.max(...arr);
+  let min = Math.min(...arr);
+  let range = max-min+1;
+  
+  let output = createArray(n);
+  let count = createArray(range);
+  let i;
+
+  for(i=0; i<n; i++)
+    count[arr[i]-min]++;
+
+  for(i=1; i<range; i++)
+    count[i] += count[i-1];
+
+  for(i=0; i<n; i++){
+    output[count[arr[i]-min] -1] = arr[i];
+    count[arr[i]-min]--;
+  }
+
+  for(i=0;i<n;i++)
+    arr[i] = output[i];
+}
+
+const arr = [-5, -10, 0, -3, 8, 5, -1, 10];
+countSort(arr, arr.length);
+console.log(arr);
